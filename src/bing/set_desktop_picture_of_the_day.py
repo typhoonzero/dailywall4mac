3f7a3175729e6adf39613d5b5f9513462e90f6fb
typhoonzero <typhoonzero1986@gmail.com>
2017-08-29 16:29:35 +0800

    some fixes

--- src/bing/set_desktop_picture_of_the_day.py
+++ src/bing/set_desktop_picture_of_the_day.py
@@ -2,6 +2,24 @@ from appscript import app, mactypes
 import os
 import time
 import subprocess
+import urllib, urllib2
+import re
+
+def download_bing_today_wallpaper():
+    wallpaper_dir = "%s/Pictures/bing-wallpapers"%os.getenv("HOME")
+    # g_img={url: "/az/hprichbg/rb/OregonPainted_ZH-CN8553728911_1920x1080.jpg"
+    fd = urllib2.urlopen("http://cn.bing.com")
+    content = fd.read()
+    fd.close()
+    matches = re.search(r"g_img\=\{url\:\ \"\/az\/hprichbg.*\.jpg\"\,", content)
+    image_string = matches.group(0)[13:-2]
+    full_image_url = "http://cn.bing.com" + image_string
+    file_name = wallpaper_dir + "/" + image_string.split("/")[-1]
+    if os.path.isfile(file_name):
+        return
+    urllib.urlretrieve(full_image_url, file_name)
+
+
 # for single monitor
 SCRIPT = """/usr/bin/osascript<<END
 tell application "Finder"
@@ -24,13 +42,19 @@ END
 def set_desktop_background(filename):
     subprocess.call(SCRIPT_MULTI % filename, shell=True)
 
-wallpaper_dir = "%s/Pictures/bing-wallpapers"%os.getenv("HOME")
-to_set_picture = ""
-for f in os.listdir(wallpaper_dir):
-    if f.endswith(".jpg"):
-        full_file_path = '/'.join([wallpaper_dir, f])
-        file_ctime = os.path.getctime(full_file_path)
-        if (time.time() - file_ctime) < 3600 * 24:
-            to_set_picture = full_file_path
-if to_set_picture:
-    set_desktop_background(to_set_picture)
+def main():
+    download_bing_today_wallpaper()
+    
+    wallpaper_dir = "%s/Pictures/bing-wallpapers"%os.getenv("HOME")
+    to_set_picture = ""
+    for f in os.listdir(wallpaper_dir):
+        if f.endswith(".jpg"):
+            full_file_path = '/'.join([wallpaper_dir, f])
+            file_ctime = os.path.getctime(full_file_path)
+            if (time.time() - file_ctime) < 3600 * 24:
+                to_set_picture = full_file_path
+    if to_set_picture:
+        set_desktop_background(to_set_picture)
+
+if __name__ == "__main__":
+    main()
